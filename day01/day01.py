ad8cf0d1415ab96c3a50fc609af4f245625a4ad4
blackerby <wmblackerby@gmail.com>
2022-12-01 09:34:12 -0600

    Update script

--- day01/day01.py
+++ day01/day01.py
@@ -6,7 +6,7 @@ data = list(map(lambda s: s.splitlines(), input))
 total_per_elf = sorted([sum([int(s) for s in lst]) for lst in data], reverse=True)
 
 # Part 1
-print(total_per_elf[0])
+print(f"Elf with most: {total_per_elf[0]}")
 
 # Part 2
-print(sum(total_per_elf[:3]))
+print(f"Top 3 elves: {sum(total_per_elf[:3])}")
